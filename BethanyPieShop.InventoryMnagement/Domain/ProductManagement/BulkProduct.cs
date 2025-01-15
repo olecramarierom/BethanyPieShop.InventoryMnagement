2ecf35a7632c2b7fea1f012635afbff482d1f867
Marcelo Moreira <MARCELO.MOREIRA@wexinc.com>
2025-01-15 15:03:36 -0300

    Add project files.

---
+++ BethanyPieShop.InventoryMnagement/Domain/ProductManagement/BulkProduct.cs
@@ -0,0 +1,29 @@
+﻿using BethanyPieShop.InventoryManagement.Domain.Contracts;
+using BethanyPieShop.InventoryMnagement.Domain.General;
+using BethanyPieShop.InventoryMnagement.Domain.ProductManagement;
+
+namespace BethanyPieShop.InventoryManagement.Domain.ProductManagement
+{
+    public class BulkProduct : Product, ISavable
+    {
+        public BulkProduct(int id, string name, string? description, Price price, UnitType unitType, int maxAmountInStock) : base(id, name, description, price, unitType, maxAmountInStock)
+        {
+
+        }
+
+        public override object Clone()
+        {
+            return new BulkProduct(0, this.Name, this.Description, new Price() { ItemPrice = this.Price.ItemPrice, Currency = Price.Currency }, this.UnitType, this.maxItemsInStock);
+        }
+
+        public string ConvertToStringForSaving()
+        {
+            return $"{Id};{Name};{Description};{maxItemsInStock};{Price.ItemPrice};{(int)Price.Currency};{(int)UnitType};3;";
+        }
+
+        public override void IncreaseStock()
+        {
+            AmountInStock++;
+        }
+    }
+}
---
+++ BethanyPieShop.InventoryMnagement/Domain/ProductManagement/FreshBoxedProduct.cs
@@ -0,0 +1,25 @@
+﻿using BethanyPieShop.InventoryMnagement.Domain.General;
+
+namespace BethanyPieShop.InventoryManagement.Domain.ProductManagement
+{
+    public class FreshBoxedProduct : BoxedProduct
+    {
+
+        #region Constructors
+        public FreshBoxedProduct(int id, 
+                                string name, 
+                                string? description, 
+                                Price price, int maxAmountInStock, int amountPerBox) 
+                                : base(id, name, description, price, maxAmountInStock, amountPerBox)
+        {
+        }
+        #endregion
+        #region Methods
+        public void UseFreshBoxedProduct(int items)
+        {
+            //UseBoxedProduct(items);
+        }
+        #endregion
+
+    }
+}
